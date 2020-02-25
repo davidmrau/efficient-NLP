61b108cc51da2f8294cab60c52f0fc950a917cd3
David Rau <david.rau@hotmail.de>
2020-02-25 13:40:23 +0100

    WIP

--- data_loader.py
+++ data_loader.py
@@ -6,8 +6,8 @@ from utils import collate_fn_padd
 def get_data_loaders(dataset_path, train_batch_size, val_batch_size, tokenizer):
 
     dataloaders = {}
-    # dataloaders['train'] = DataLoader(MSMarco(dataset_path, 'train'),
-    #     batch_size=train_batch_size, collate_fn=collate_fn_padd)
+    dataloaders['train'] = DataLoader(MSMarcoTrain(dataset_path),
+    batch_size=train_batch_size, collate_fn=collate_fn_padd)
     dataloaders['test'] =  DataLoader(MSMarcoDev(dataset_path),
         batch_size=val_batch_size, collate_fn=collate_fn_padd)
 
--- dataset.py
+++ dataset.py
@@ -11,10 +11,8 @@ class MSMarcoTrain(data.Dataset):
 
 
         self.triplets = read_triplets(f'{dataset_path}/qidpidtriples.{split}.full.tsv')
-        self.docs_path = path.join(dataset_path, 'collection/')
-        self.queries_path = path.join(dataset_path, f'queries.{split}/')
-        self.doc_ids = np.load(f'{self.docs_path}ids.npy')
-
+        self.docs = read_pickle(f'{dataset_path}/collection.tsv.p')
+        self.queries = read_pickle(f'{dataset_path}/queries.{split}.tsv.p')
 
     def __len__(self):
         # double size, because dataset contain only relevant examples
@@ -24,9 +22,9 @@ class MSMarcoTrain(data.Dataset):
 
         q_id, d1_id, d2_id = self.triplets[index]
 
-        query = np.load(f'{self.docs_path}{q_id}.npy')
-        doc1 = np.load(f'{self.docs_path}{d1_id}.npy')
-        doc2 = np.load(f'{self.docs_path}{d2_id}.npy')
+        query = self.queries[q_id]
+        doc1 = self.docs[d1_id]
+        doc2 = self.docs[d2_id]
 
         if np.random.random() > 0.5:
             return [query, doc1, doc2], 1
@@ -40,28 +38,28 @@ class MSMarcoDev(data.Dataset):
 
 
         self.qrels = read_qrels(path.join(dataset_path, f'qrels.{split}.tsv'))
-        self.docs_path = path.join(dataset_path, 'collection/')
-        self.queries_path = path.join(dataset_path, f'queries.{split}/')
-        self.doc_ids = np.load(f'{self.docs_path}ids.npy')
+        self.docs = read_pickle(f'{dataset_path}/collection.tsv.p')
+        self.queries = read_pickle(f'{dataset_path}/queries.{split}.tsv.p')
+        self.doc_ids = np.load(f'{dataset_path}/doc_ids.npy')
 
 
     def __len__(self):
         # double size, because dataset contain only relevant examples
-        return self.qrels.shape[0]
+        return len(self.qrels)
 
     def __getitem__(self, index):
 
         q_id, d1_id = self.qrels[index]
 
         d2_id = np.random.choice(self.doc_ids)
-
         # making sure that the sampled d1_id is diferent from relevant d2_id
         while d1_id == d2_id:
             d2_id = np.random.choice(self.doc_ids)
 
-        query = np.load(f'{self.docs_path}{q_id}.npy')
-        doc1 = np.load(f'{self.docs_path}{d1_id}.npy')
-        doc2 = np.load(f'{self.docs_path}{d2_id}.npy')
+
+        query = self.queries[q_id]
+        doc1 = self.docs[d1_id]
+        doc2 = self.docs[d2_id]
 
         if np.random.random() > 0.5:
             return [query, doc1, doc2], 1
