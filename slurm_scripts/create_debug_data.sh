2b1d0835061347276ce925efb736cacda9349500
davidmrau <draugpu@test3.irc.sara.nl.irc.sara.nl>
2020-03-02 11:03:00 +0100

    add slurm scripts

---
+++ slurm_scripts/create_debug_data.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+#SBATCH --job-name=train_snrm
+#SBATCH --nodes=1 --ntasks-per-node=1
+#SBATCH --time=01:00:00
+cd ..
+python3 create_bebug_data.py
---
+++ slurm_scripts/gpu_train_tf.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+#SBATCH --job-name=train_tf
+#SBATCH --nodes=1 
+#SBATCH -p gpu_shared
+#SBATCH --time=12:00:00
+cd ..
+python3 main.py model=tf
