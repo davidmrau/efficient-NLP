7c610853a1318cda3efed4850e832566fff1a4c7
davidmrau <draugpu@test3.irc.sara.nl.irc.sara.nl>
2020-03-05 18:00:48 +0100

    update main

--- slurm_scripts/main.sh
+++ slurm_scripts/main.sh
@@ -6,14 +6,14 @@ QUERY_FILE=msmarco-test2019-queries.tokenized.tsv
 QRELS=qrels.dev.tsv
 
 
-
+EXPERIMENT_FOLDER='experiments/'
 
 echo ${QRELS}
 echo ${QUERY_FILE}
 echo ${DOCS_FILE}
 
 for HIDDEN in 100 200;do
-	MODEL_FOLDER=${HIDDEN}_model
+	MODEL_FOLDER=${EXPERIMENT_FOLDER}${HIDDEN}_model
 	echo ${MODEL_FOLDER} 
 	python3 main.py model_folder=${MODEL_FOLDER}
 	python3 create_index.py model_folder=${MODEL_FOLDER} docs_file=${DOCS_FILE}
