30ab471701cad50691f4e3f81e7fdd7d322cc873
kondilidisn <kondilidisn9@gmail.com>
2020-03-05 19:16:25 +0100

    add filid drive

--- slurm_scripts/startup_script.sh
+++ slurm_scripts/startup_script.sh
@@ -1,6 +1,6 @@
 
 # download data
-wget --no-check-certificate -r 'https://docs.google.com/uc?export=download&id=FILEID' -O ../data.tar.gz
+wget --no-check-certificate -r 'https://docs.google.com/uc?export=download&id=13FlxE2GN9pMAc7K0-iT8YWqVGkGUZZ43' -O ../data.tar.gz
 tar -xzf data.tar.gz -C ../
 
 pip3 install -r ../requirements.txt --user
