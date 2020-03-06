eb3533d312fb570b23738a58f95e3712cbfdc016
davidmrau <david.rau@hotmail.de>
2020-03-06 17:38:00 +0100

    change download from cloud

--- slurm_scripts/startup.sh
+++ slurm_scripts/startup.sh
@@ -1,7 +1,13 @@
+fileId=13FlxE2GN9pMAc7K0-iT8YWqVGkGUZZ43
+fileName=data.tar.gz
+curl -sc /tmp/cookie "https://drive.google.com/uc?export=download&id=${fileId}" > /dev/null
+code="$(awk '/_warning_/ {print $NF}' /tmp/cookie)"  
+curl -Lb /tmp/cookie "https://drive.google.com/uc?export=download&confirm=${code}&id=${fileId}" -o ${fileName} 
+
 
 # download data
-wget --no-check-certificate -r 'https://docs.google.com/uc?export=download&id=13FlxE2GN9pMAc7K0-iT8YWqVGkGUZZ43' -O data.tar.gz
-tar -xzf data.tar.gz -C ../
+tar -xzf data.tar.gz
+mv data ../
 rm data.tar.gz
 
 pip3 install -r ../requirements.txt --user
