eab2c09b8e36895bc537c1184081dc5bd9f9a4f9
kondilidisn <kondilidisn9@gmail.com>
2020-03-11 15:03:24 +0100

    test_train.sh

---
+++ slurm_scripts/test_train.sh
@@ -0,0 +1,41 @@
+
+QUERY_FILE='msmarco-test2019-queries-43-judged.tokenized.tsv'
+QRELS='2019qrels-pass_without_q_tabs.txt'
+DOCS_FILE='msmarco-passagetest2019-top1000_43.tsv.d_id_doc.tokenized_uniq.tsv'
+
+
+cd ..
+# Experiment parameters:
+EXPERIMENT_FOLDER='experiments/'
+
+EMBEDDINGS="bert"
+SPARSE_DIMENSIONS="400"
+L1_SCALARS="1"
+BATCH_SIZES="64"
+
+# Model parameters:
+MODELS="snrm"
+# SNRM parameters:
+SNRM_HIDDENS="200"
+
+for EMBEDDING in ${EMBEDDINGS}; do
+	for SPARSE_DIMENSION in ${SPARSE_DIMENSIONS}; do
+		for L1_SCALAR in ${L1_SCALARS}; do
+			for BATCH_SIZE in ${BATCH_SIZES}; do
+
+				MODEL=snrm
+
+				for SNRM_HIDDEN in ${SNRM_HIDDENS}; do
+					MODEL_STRING=${MODEL}_${SNRM_HIDDEN}
+
+					EXP_DIR=${EXPERIMENT_FOLDER}l1_${L1_SCALAR}_Emb_${EMBEDDING}_Sparse_${SPARSE_DIMENSION}_bsz_${BATCH_SIZE}_${MODEL_STRING}
+
+					echo ${EXP_DIR}
+					# echo "Training"
+					python3 main.py model_folder=${EXP_DIR} query_file=${QUERY_FILE} qrels=${QRELS} docs_file=${DOCS_FILE}
+
+				done
+			done
+		done
+	done
+done
