0bae2033956ba45f7b4598e61a400ccdefd7a490
kondilidisn <kondilidisn9@gmail.com>
2020-03-13 15:27:59 +0100

    test batch size for 1 epoch

--- slurm_scripts/test_bsz_snrm.sh
+++ slurm_scripts/test_bsz_snrm.sh
@@ -1,10 +1,10 @@
 
 MODEL=snrm
 
-BATCH_SIZE="1024"
+BATCH_SIZE="512"
 
 cd ..
 
 # test what is the max batch size that can fit in the gpu. If the folloing trains at least one epoch, we are good for the rest experiments that follow
 python3 main.py model=snrm batch_size=${BATCH_SIZE} embedding=bert sparse_dimensions=10000 l1_scalar=0 snrm.hidden_sizes=500-300-100-300-500 \
-model_folder=Test_Batch_size_exp
+model_folder=Test_Batch_size_exp num_epochs=1
