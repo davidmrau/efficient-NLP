0e562e96ad70a897e8898015a8efd127d779e740
kondilidisn <kondilidisn9@gmail.com>
2020-03-13 12:45:46 +0100

    bsz test

---
+++ slurm_scripts/test_bsz_tf.sh
@@ -0,0 +1,12 @@
+
+
+MODEL=tf
+
+BATCH_SIZES="1024"
+
+cd ..
+
+# test what is the max batch size that can fit in the gpu. If the folloing trains at least one epoch, we are good for the rest experiments that follow
+python3 main.py model=${MODEL} batch_size=${BATCH_SIZE} embedding=bert sparse_dimensions=10000 l1_scalar=0 \
+tf.num_of_layers=8 tf.num_attention_heads=8 tf.hidden_size=768 tf.pooling_method=CLS  \
+model_folder=Test_Batch_size_exp
