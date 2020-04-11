a1fe19c13c02d653a252269bcf5ead98459578fb
kondilidisn <kondilidisn9@gmail.com>
2020-04-11 21:29:31 +0200

    updated experiment scripts tf and hyper-snrm

--- slurm_scripts/hyper-snrm.sh
+++ slurm_scripts/hyper-snrm.sh
@@ -28,11 +28,11 @@ cd ..
 MODEL=snrm
 
 
-EMBEDDINGS="bert glove"
+EMBEDDINGS="bert"
 L1_SCALARS="0 0.1 1"
 SPARSE_DIMENSIONS="1000 5000"
 N_GRAM_MODELS="cnn bert"
-LARGE_OUT_BIASES_OPTIONS="True False"
+LARGE_OUT_BIASES_OPTIONS="False"
 
 # Dense EMB-500-100-500
 SNRM_HIDDEN="256-768"
@@ -43,7 +43,32 @@ for EMBEDDING in ${EMBEDDINGS}; do
 		for SPARSE_DIMENSION in ${SPARSE_DIMENSIONS}; do
 			for N_GRAM_MODEL in ${N_GRAM_MODELS}; do
 				for LARGE_OUT_BIASES in ${LARGE_OUT_BIASES_OPTIONS}; do
-					python3 main.py model=${MODEL} batch_size=${BATCH_SIZE} embedding=${EMBEDDING} sparse_dimensions=${SPARSE_DIMENSION} l1_scalar=${L1_SCALAR} snrm.hidden_sizes=${SNRM_HIDDEN} n_gram_model=${N_GRAM_MODEL} large_out_biases=${LARGE_OUT_BIASES}
+					python3 main.py model=${MODEL} batch_size=${BATCH_SIZE} embedding=${EMBEDDING} sparse_dimensions=${SPARSE_DIMENSION} l1_scalar=${L1_SCALAR} snrm.hidden_sizes=${SNRM_HIDDEN} snrm.n_gram_model=${N_GRAM_MODEL} large_out_biases=${LARGE_OUT_BIASES}
+				done
+			done
+		done
+	done
+done
+
+
+EMBEDDINGS="glove"
+L1_SCALARS="0 0.1 1"
+SPARSE_DIMENSIONS="1000 5000"
+N_GRAM_MODELS="cnn bert"
+LARGE_OUT_BIASES_OPTIONS="False"
+
+# Dense EMB-500-100-500
+SNRM_HIDDEN_OPTIONS="256-768 100-500"
+
+for SNRM_HIDDEN in ${SNRM_HIDDEN_OPTIONS}; do
+	for EMBEDDING in ${EMBEDDINGS}; do
+
+		for L1_SCALAR in ${L1_SCALARS}; do
+			for SPARSE_DIMENSION in ${SPARSE_DIMENSIONS}; do
+				for N_GRAM_MODEL in ${N_GRAM_MODELS}; do
+					for LARGE_OUT_BIASES in ${LARGE_OUT_BIASES_OPTIONS}; do
+						python3 main.py model=${MODEL} batch_size=${BATCH_SIZE} embedding=${EMBEDDING} sparse_dimensions=${SPARSE_DIMENSION} l1_scalar=${L1_SCALAR} snrm.hidden_sizes=${SNRM_HIDDEN} snrm.n_gram_model=${N_GRAM_MODEL} large_out_biases=${LARGE_OUT_BIASES}
+					done
 				done
 			done
 		done
--- slurm_scripts/tf.sh
+++ slurm_scripts/tf.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Set job requirements
-#SBATCH --job-name=tf_4H_0l1
+#SBATCH --job-name=tf_s_bs
 #SBATCH --ntasks=1
 #SBATCH --partition=gpu
 #SBATCH --gres=gpu:4
@@ -19,7 +19,7 @@ module load Python/3.6.3-foss-2017b
 module load cuDNN/7.0.5-CUDA-9.0.176
 module load NCCL/2.0.5-CUDA-9.0.176
 
-BATCH_SIZE="512"
+BATCH_SIZES="128 256"
 
 
 
@@ -34,42 +34,41 @@ EXPERIMENT_FOLDER='experiments/'
 
 # Transformer parameters:
 TF_LAYERS="2"
-TF_HEADS="4"
+TF_HEADS="4 8"
 # TF_HID_DIMS="256 512"
 TF_POOLS="AVG"
-EMBEDDINGS="bert"
+EMBEDDINGS="bert glove"
 
-L1_SCALARS="0"
-
-
+L1_SCALARS="0 1"
 
 
-SPARSE_DIMENSIONS="1000 5000 10000"
-
-for EMBEDDING in ${EMBEDDINGS}; do
-	for SPARSE_DIMENSION in ${SPARSE_DIMENSIONS}; do
-		for L1_SCALAR in ${L1_SCALARS}; do
-
-			if [ $EMBEDDING == bert ]; then
-			  TF_HID_DIMS=768
-			fi
-
-			if [ $EMBEDDING == glove ]; then
-			  TF_HID_DIMS=300
-			fi
+SPARSE_DIMENSIONS="10000"
+LARGE_OUT_BIASES_OPTIONS="False True"
 
 
-			for TF_LAYER in ${TF_LAYERS}; do
-				for TF_HEAD in ${TF_HEADS}; do
-					for TF_HID_DIM in ${TF_HID_DIMS}; do
+for BATCH_SIZE in ${BATCH_SIZES}; do 
+	
+	for EMBEDDING in ${EMBEDDINGS}; do
+		
+		for SPARSE_DIMENSION in ${SPARSE_DIMENSIONS}; do
+			
+			for L1_SCALAR in ${L1_SCALARS}; do
+				
+
+				for TF_LAYER in ${TF_LAYERS}; do
+					
+					for TF_HEAD in ${TF_HEADS}; do
+						
+						
 						for TF_POOL in ${TF_POOLS}; do
+							
 
-							echo ${EXP_DIR}
-							# echo "Training"
-							python3 main.py model=${MODEL} batch_size=${BATCH_SIZE} embedding=${EMBEDDING} sparse_dimensions=${SPARSE_DIMENSION} l1_scalar=${L1_SCALAR} \
-							tf.num_of_layers=${TF_LAYER} tf.num_attention_heads=${TF_HEAD} tf.hidden_size=${TF_HID_DIM} tf.pooling_method=${TF_POOL}
+							for LARGE_OUT_BIASES in ${LARGE_OUT_BIASES_OPTIONS}; do
 
+								python3 main.py model=${MODEL} batch_size=${BATCH_SIZE} embedding=${EMBEDDING} sparse_dimensions=${SPARSE_DIMENSION} l1_scalar=${L1_SCALAR} \
+								tf.num_of_layers=${TF_LAYER} tf.num_attention_heads=${TF_HEAD} tf.hidden_size=${TF_HID_DIM} tf.pooling_method=${TF_POOL} large_out_biases=${LARGE_OUT_BIASES}
 
+							done
 						done
 					done
 				done
