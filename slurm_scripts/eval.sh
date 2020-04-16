9a90667dc9670f01b911a1f26d3a4a65ebf4bd65
davidmrau <david.rau@hotmail.de>
2020-04-16 16:09:07 +0200

    update eval script

--- slurm_scripts/eval.sh
+++ slurm_scripts/eval.sh
@@ -2,7 +2,8 @@
 # Set job requirements
 #SBATCH --job-name=eval
 #SBATCH --ntasks=1
-#SBATCH --partition=gpu_shared
+#SBATCH --partition=gpu
+#SBATCH --gres=gpu:4
 #SBATCH --time=01:00:00
 #SBATCH --mem=100G
 
@@ -22,8 +23,8 @@ QRELS_PATH="data/msmarco/2019qrels-pass_filtered_ms_marco.txt"
 
 QUERY_DOCS_PATH="data/msmarco/msmarco-passagetest2019-top1000.tsv.sorted"
 
-MODEL_PATH="experiments/l1_0_Emb_bert_Sparse_1000_bsz_512_lr_0.0001_TF_L_2_H_4_D_768_P_AVG"
-
+#MODEL_PATH="experiments/l1_0_Emb_bert_Sparse_1000_bsz_512_lr_0.0001_TF_L_2_H_4_D_768_P_AVG"
+MODEL_PATH='experiments/l1_1_Emb_bert_Sparse_10000_bsz_256_lr_0.0001_TF_L_2_H_4_D_768_P_AVG_large_out_biases/'
 
 
 cd ..
