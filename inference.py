7c03e318f94b1f972dd2336860fb05e6f685f08b
davidmrau <david.rau@hotmail.de>
2020-04-17 09:47:55 +0200

    add num used dimensions to eval

--- inference.py
+++ inference.py
@@ -147,7 +147,7 @@ def inference(cfg):
 	q_ids = q_ids_q
 	av_l0_query.append(l0_loss(torch.cat(q_repr)))
 	av_l0_doc.append(l0_loss(torch.cat(d_repr)))
-	
+	count = 0	
 	while len(d_repr) > 0:
 		print(q_ids_q)
 		scores_q, q_repr, d_repr, q_ids_q, d_ids_q = evaluate(model, data_loaders, device, cfg.top_results, reset=False)
@@ -156,10 +156,15 @@ def inference(cfg):
 		if len(d_repr) > 0:
 			av_l0_query.append(l0_loss(torch.cat(q_repr)))
 			av_l0_doc.append(l0_loss(torch.cat(d_repr)))
+		count += 1
+		if count > 5:
+			break
 	
 	write_ranking(scores, q_ids, ranking_file_path, cfg.MaxMRRRank)
 	write_ranking_trec(scores, q_ids, ranking_file_path+'.trec', cfg.MaxMRRRank)
 
+	metrics_file.write(f'{qrels_base} dim used docs:\t{int(torch.stack(av_l0_doc).mean().item() * cfg.sparse_dimensions)}\n')
+	metrics_file.write(f'{qrels_base} dim used query:\t{int(torch.stack(av_l0_query).mean().item() * cfg.sparse_dimensions)}\n')
 
 	metrics_file.write(f'{qrels_base} l0 docs:\t{round(torch.stack(av_l0_query).mean().item(), 5)}\n')
 	metrics_file.write(f'{qrels_base} l0 query:\t{round(torch.stack(av_l0_doc).mean().item(), 5)}\n')
