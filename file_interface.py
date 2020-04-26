329ee7462aba25b0e375a57b1622944d40217f99
kondilidisn <kondilidisn9@gmail.com>
2020-04-26 16:12:28 +0200

    FileInterface is multiprocessing safe. Now training can be done with as many threads specified in num_workers param

--- file_interface.py
+++ file_interface.py
@@ -2,6 +2,7 @@ from utils import *
 import numpy as np
 
 
+import multiprocessing
 
 
 class FileInterface:
@@ -13,20 +14,23 @@ class FileInterface:
 			the dictionary matches element ids (doc_id / query_id / line_id for triplets) to their seek value within the file
 	"""
 
-	def __init__(self, filename):
-			# check if filename exists
-			try:
-				# we keep the file always open
-				self.file = open(filename, 'r')
-			except:
-				raise IOError(f'File: {filename} not accessible!')
+	# A class variable in order to make file reading multiprocess safe
+	lock = multiprocessing.Lock()
 
-			# check if dictionary pickle exists
-			try:
-					# given an id of an element, this dictionary returns the seek value of the file to the corresponding line
-				self.seek_dict = read_pickle(filename + '.offset_dict.p')
-			except:
-				raise IOError(f'File: {filename}.offset_dict.p not accessible!\nYou need to first create the dictionary with seek values for this file!!\nCheck offset_dict.py')
+	def __init__(self, filename):
+		# check if filename exists
+		try:
+			# we keep the file always open
+			self.file = open(filename, 'r')
+		except:
+			raise IOError(f'File: {filename} not accessible!')
+
+		# check if dictionary pickle exists
+		try:
+				# given an id of an element, this dictionary returns the seek value of the file to the corresponding line
+			self.seek_dict = read_pickle(filename + '.offset_dict.p')
+		except:
+			raise IOError(f'File: {filename}.offset_dict.p not accessible!\nYou need to first create the dictionary with seek values for this file!!\nCheck offset_dict.py')
 
 
 
@@ -35,6 +39,8 @@ class FileInterface:
 		return len(self.seek_dict)
 
 	def get(self, id):
+
+		with FileInterface.lock:
 			# seek the file to the line that you want to read
 			self.file.seek( self.seek_dict[id] )
 			# read and return the line
@@ -42,18 +48,18 @@ class FileInterface:
 
 	def get_triplet(self, index):
 		# returns 3 strings q_id, d1_id, d2_id
-			return self.get(index).strip().split('\t')
+		return self.get(index).strip().split('\t')
 
 
 	def get_tokenized_element(self, id):
-			""" Used to read one Document OR Query from file, given the
-			"""
-			# return (str) element_id, and numpy array of token ids
-			line = self.get(id)
-			# getting position of first ' ' that separates the doc_id and the begining of the token ids
-			delim_pos = line.find(' ')
-			# extracting the id
-			# id = line[:delim_pos]
-			# extracting the token_ids and creating a numpy array
-			tokens_list = np.fromstring(line[delim_pos+1:], dtype=int, sep=' ')
-			return tokens_list
+		""" Used to read one Document OR Query from file, given the
+		"""
+		# return (str) element_id, and numpy array of token ids
+		line = self.get(id)
+		# getting position of first ' ' that separates the doc_id and the begining of the token ids
+		delim_pos = line.find(' ')
+		# extracting the id
+		# id = line[:delim_pos]
+		# extracting the token_ids and creating a numpy array
+		tokens_list = np.fromstring(line[delim_pos+1:], dtype=int, sep=' ')
+		return tokens_list
