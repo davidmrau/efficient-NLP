c795f4f9b2bfe2f71e019a0d9b06be18d9b3a4be
kondilidisn <kondilidisn9@gmail.com>
2020-04-27 20:40:45 +0200

    Created Tokenizer class, updated tokenizer.py script

--- tokenizer.py
+++ tokenizer.py
@@ -5,24 +5,40 @@ from nltk import word_tokenize
 import pickle
 tokenizer_bert = BertTokenizer.from_pretrained('bert-base-uncased')
 
+from text_prepro import Tokenizer
+
 
 
 
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--delimiter', type=str, default='\t')
-parser.add_argument('--folder', type=str)
-parser.add_argument('--fname', type=str)
-parser.add_argument('--max_len', type=int)
-parser.add_argument('--word2index_path', type=str, default='../data/embeddings/glove.6B.300d_word2idx_dict.p')
-parser.add_argument('--whitespace', action='store_true')
+parser.add_argument('--input_file', type=str)
+parser.add_argument('--max_len', default=-1, type=int)
+parser.add_argument('--word2index_path', type=str, default='data/embeddings/glove.6B.300d_word2idx_dict.p')
+parser.add_argument('--tokenizer', type=str, help = "{'bert','glove'}")
+parser.add_argument('--stopwords', type=str, default="none", help = "{'none','lucene', 'some/path/file'}")
+parser.add_argument('--remove_unk', action='store_true')
 args = parser.parse_args()
 
-in_fname = args.folder + args.fname
-base = os.path.splitext(in_fname)[0]
-add = 'glove' if args.whitespace else 'bert'
-out_fname = f'{base}.{add}.tsv'
 
+
+tokenizer = Tokenizer(tokenizer = args.tokenizer, max_len = args.max_len, stopwords=args.stopwords, remove_unk = args.remove_unk,
+						word2index_path = args.word2index_path, lower_case = True, stemmer = None)
+
+
+
+in_fname = args.input_file
+
+print(in_fname)
+base = os.path.splitext(in_fname)[0]
+
+# print(base)
+
+# add = 'glove' if args.whitespace else 'bert'
+out_fname = f'{base}.{args.tokenizer}.stop_{args.stopwords}{".remove_unk" if args.remove_unk else ""}.len_{args.max_len}.tsv'
+
+print(out_fname)
 word2idx = pickle.load(open(args.word2index_path, 'rb')) 
 with open(out_fname, 'w') as out_f:
 	with open(in_fname, 'r') as in_f:
@@ -34,18 +50,24 @@ with open(out_fname, 'w') as out_f:
 		line = in_f.readline()
 		while line:
 
-			if count % 10000 == 0:
+			if count % 1000 == 0 and count != 0:
 				print(f'lines read: {count}')
+
+
 			id_, text = line.strip().split(args.delimiter, 1)
-			if not args.whitespace:
-				tokenized_ids = tokenizer_bert.encode(text, max_length = args.max_len)
-			else:
-				tokenized_ids = list()
-				for word in word_tokenize(line)[:args.max_len]:
-					try:
-						tokenized_ids.append(word2idx[word.lower()])
-					except:
-						pass
+
+			tokenized_ids = tokenizer.encode(text)
+			# if not args.whitespace:
+			# 	tokenized_ids = tokenizer_bert.encode(text, max_length = args.max_len)
+			# else:
+			# 	tokenized_ids = list()
+			# 	for word in word_tokenize(line)[:args.max_len]:
+			# 		try:
+			# 			tokenized_ids.append(word2idx[word.lower()])
+			# 		except:
+			# 			pass
+
+
 			out_f.write(id_ + ' ' + ' '.join(str(t) for t in tokenized_ids) + '\n')
 			count += 1
 			line = in_f.readline()
