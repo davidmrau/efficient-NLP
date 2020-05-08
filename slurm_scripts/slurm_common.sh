1035e5ea7e86aef57eecef0bef6251d4bd3ecd64
davidmrau <david.rau@hotmail.de>
2020-05-08 20:40:54 +0200

    add slurm ease

---
+++ slurm_scripts/slurm_common.sh
@@ -0,0 +1,14 @@
+USER=`accinfo | grep NRC | awk '{print $3}'`
+#Loading modules
+module purge
+module load pre2019
+module load eb
+module load Python/3.6.3-foss-2017b
+module load cuDNN/7.0.5-CUDA-9.0.176
+module load NCCL/2.0.5-CUDA-9.0.176 
+
+cd ..
+bash telegram.sh "${USER} ${1} ${SLURM_JOBID} Started"
+source slurm_scripts/${1}
+bash telegram.sh "${USER} ${1} ${SLURM_JOBID} Finished"  
+
