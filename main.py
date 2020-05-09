6358ff3f81106e16f10f4520a783d1ca3b2c6476
kondilidisn <kondilidisn9@gmail.com>
2020-05-09 13:35:43 +0200

    updated print for extra workers on main

--- main.py
+++ main.py
@@ -50,7 +50,7 @@ def exp(cfg):
 	elif cfg.num_workers > avail_threads:
 		cfg.num_workers = avail_threads
 
-	print(f"Using {cfg.num_workers} Threads.")
+	print(f"Using {cfg.num_workers} exra thread(s) for each dataset.")
 
 	print(model)
 	# initialize tensorboard
@@ -80,6 +80,7 @@ def exp(cfg):
 	cfg.model_folder, l1_scalar=cfg.l1_scalar, balance_scalar=cfg.balance_scalar, patience = cfg.patience,
 	samples_per_epoch_train = cfg.samples_per_epoch_train, samples_per_epoch_val=cfg.samples_per_epoch_val, bottleneck_run = cfg.bottleneck_run,
 	log_every_ratio = cfg.log_every_ratio, max_rank = cfg.max_rank, metric = metric, sparse_dimensions = cfg.sparse_dimensions)
+
 
 
 
