aa3fd54b83c984246ea9073cf6be7d8274b582ee
davidmrau <david.rau@hotmail.de>
2020-05-18 22:55:33 +0200

    update slurm scripts

--- slurm_scripts/slurm_common.sh
+++ slurm_scripts/slurm_common.sh
@@ -2,6 +2,7 @@ USER=`accinfo | grep NRC | awk '{print $3}'`
 #Loading modules
 module purge
 module load pre2019
+module load 2019
 module load eb
 module load Python/3.6.3-foss-2017b
 module load cuDNN/7.0.5-CUDA-9.0.176
