ef7412d6d7a15c030796e4656868a1097c818f38
davidmrau <david.rau@hotmail.de>
2020-05-26 16:43:54 +0200

    load python 3.6.6

--- slurm_scripts/slurm_common.sh
+++ slurm_scripts/slurm_common.sh
@@ -4,7 +4,8 @@ module purge
 module load pre2019
 module load 2019
 module load eb
-module load Python/3.6.3-foss-2017b
+module load Python/3.6.6-foss-2019b
+#module load Python/3.7.5-foss-2019b
 module load cuDNN/7.0.5-CUDA-9.0.176
 module load NCCL/2.0.5-CUDA-9.0.176 
 
@@ -12,6 +13,8 @@ cd ..
 
 FILE_NAME=$1
 export FILE_NAME
+
+
 
 bash telegram.sh "${USER} ${FILE_NAME} ${SLURM_JOBID} Started"
 source slurm_scripts/${1}
