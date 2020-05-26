95115c72653876f0418a9279caaec81cc0d8ccff
kondilidisn <kondilidisn9@gmail.com>
2020-05-26 12:43:28 +0200

    updated test.sh

--- slurm_scripts/test.sh
+++ slurm_scripts/test.sh
@@ -21,18 +21,24 @@ module load NCCL/2.0.5-CUDA-9.0.176
 BATCH_SIZE="64"
 
 cd ..
-DATASET='robust04'
-STOPWORDS='lucene'
+DATASET='msmarco'
+STOPWORDS='none'
 
-MODEL=snrm
+MODEL=bert
 
-EMBEDDINGS="glove"
+EMBEDDINGS="bert"
 L1_SCALARS="0"
 SPARSE_DIMENSIONS="5000"
 N_GRAM_MODELS="cnn"
 
+# Transformer parameters:
+TF_LAYERS="2"
+TF_HEADS="8"
+# TF_HID_DIMS="256 512"
+TF_POOLS="AVG"
+
 # Dense EMB-500-100-500
-SNRM_HIDDEN="100-300"
+# SNRM_HIDDEN="100-300"
 
 for EMBEDDING in ${EMBEDDINGS}; do
 
@@ -42,26 +48,13 @@ for EMBEDDING in ${EMBEDDINGS}; do
 
 				
 				python3 main.py model=${MODEL} batch_size=${BATCH_SIZE} embedding=${EMBEDDING} sparse_dimensions=${SPARSE_DIMENSION} \
-				l1_scalar=${L1_SCALAR} snrm.hidden_sizes=${SNRM_HIDDEN} n_gram_model=${N_GRAM_MODEL} dataset=${DATASET} \
-				samples_per_epoch_train=1000 samples_per_epoch_val=128 stopwords=${STOPWORDS} num_epochs=1 num_workers=2 \
-				sampler="uniform" target="binary"
-
-				python3 main.py model=${MODEL} batch_size=${BATCH_SIZE} embedding=${EMBEDDING} sparse_dimensions=${SPARSE_DIMENSION} \
-				l1_scalar=${L1_SCALAR} snrm.hidden_sizes=${SNRM_HIDDEN} n_gram_model=${N_GRAM_MODEL} dataset=${DATASET} \
-				samples_per_epoch_train=1000 samples_per_epoch_val=128 stopwords=${STOPWORDS} num_epochs=1 num_workers=2 \
-				sampler="uniform" target="rank_prob"
-
-				python3 main.py model=${MODEL} batch_size=${BATCH_SIZE} embedding=${EMBEDDING} sparse_dimensions=${SPARSE_DIMENSION} \
-				l1_scalar=${L1_SCALAR} snrm.hidden_sizes=${SNRM_HIDDEN} n_gram_model=${N_GRAM_MODEL} dataset=${DATASET} \
-				samples_per_epoch_train=1000 samples_per_epoch_val=128 stopwords=${STOPWORDS} num_epochs=1 num_workers=2 \
-				sampler="zipf" target="binary"
-
-				python3 main.py model=${MODEL} batch_size=${BATCH_SIZE} embedding=${EMBEDDING} sparse_dimensions=${SPARSE_DIMENSION} \
-				l1_scalar=${L1_SCALAR} snrm.hidden_sizes=${SNRM_HIDDEN} n_gram_model=${N_GRAM_MODEL} dataset=${DATASET} \
-				samples_per_epoch_train=1000 samples_per_epoch_val=128 stopwords=${STOPWORDS} num_epochs=1 num_workers=2 \
-				sampler="zipf" target="rank_prob"
-
+				l1_scalar=${L1_SCALAR} samples_per_epoch_train=1000 samples_per_epoch_val=128 stopwords=${STOPWORDS} num_epochs=1 num_workers=2 \
+				dataset=${DATASET} load_bert_layers=0-1-2-3-4-5-6-7-8-9-10-11-12-13-14 load_bert_path="experiments_msmarco/lm_batch_8/best_model_cpu.model" \
+				tf.num_of_layers=${TF_LAYER} tf.num_attention_heads=${TF_HEAD} tf.hidden_size=${TF_HID_DIM} tf.pooling_method=${TF_POOL}
+				
 			done
 		done
 	done
 done
+
+
