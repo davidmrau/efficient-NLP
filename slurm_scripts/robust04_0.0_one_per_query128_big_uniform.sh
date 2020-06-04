1f6ba397ad93c04d7b3baaf03498f816bc8f8888
kondilidisn <kondilidisn9@gmail.com>
2020-06-04 15:10:13 +0200

    updated scripts for Mostafa

--- slurm_scripts/robust04_0.0_one_per_query128_big_uniform.sh
+++ slurm_scripts/robust04_0.0_one_per_query128_big_uniform.sh
@@ -7,8 +7,8 @@ STOPWORDS='lucene'
 MODEL=snrm
 EMBEDDINGS="glove"
 
-TRAIN_SAMPLES="100000"
-VAL_SAMPLES="10000"
+TRAIN_SAMPLES="500000"
+VAL_SAMPLES="20000"
 NUM_EPOCHS="100000"
 
 
@@ -34,7 +34,7 @@ for EMBEDDING in ${EMBEDDINGS}; do
 				python3 main.py model=${MODEL} batch_size_train=${BATCH_SIZE} embedding=${EMBEDDING} sparse_dimensions=${SPARSE_DIMENSION} l1_scalar=${L1_SCALAR} \
 				snrm.hidden_sizes=${SNRM_HIDDEN} n_gram_model=${N_GRAM_MODEL} dataset=${DATASET} samples_per_epoch_train=${TRAIN_SAMPLES} \
 				samples_per_epoch_val=${VAL_SAMPLES} stopwords=${STOPWORDS} num_epochs=${NUM_EPOCHS} num_workers=1 samples_per_query=${SAMPLES_PER_QUERY} sampler=${SAMPLER} \
-				num_workers=1 patience=30 single_sample=True
+				num_workers=1 patience=5 single_sample=True telegram=True
 
 			done
 		done
