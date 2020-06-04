7a9357ccaa01d410feb79afa02e01bd72e4a55d8
davidmrau <david.rau@hotmail.de>
2020-06-04 22:27:11 +0200

    update file in download

--- slurm_scripts/startup.sh
+++ slurm_scripts/startup.sh
@@ -2,7 +2,6 @@
 chmod u+x telegram.sh
 # download data
 
-DOWNLOAD_FOLDER=data/
 PREFIX=https://rijsbergen.hum.uva.nl/david/
 DOWNLOAD_FILE=slurm_scripts/download_file_list
 
@@ -10,6 +9,9 @@ pip3 install -r requirements.txt --user
 
 while read file;do
 	DOWNLOAD_FOLDER=`echo ${file} | sed 's|\(.*/\).*|\1|'`
-        wget ${PREFIX}${file} -P ${DOWNLOAD_FOLDER}
+	if [ ! -f "${file}" ]; then
+        	wget ${PREFIX}${file} -P ${DOWNLOAD_FOLDER}
+	fi
+
 done < $DOWNLOAD_FILE
 
