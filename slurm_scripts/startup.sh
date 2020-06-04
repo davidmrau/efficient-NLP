4faf619b9d8b5b047700212daa2cb5e8dcf96af2
davidmrau <david.rau@hotmail.de>
2020-06-04 14:53:12 +0200

    update startup

--- slurm_scripts/startup.sh
+++ slurm_scripts/startup.sh
@@ -1,22 +1,11 @@
-<<<<<<< Updated upstream
 #!/bin/bash
 chmod u+x ../telegram.sh
 # download data
-=======
-PREFIX=https://rijsbergen.hum.uva.nl/david/
-DOWNLOAD_FILE=download_file_list
-
-chmod u+x ../telegram.sh
->>>>>>> Stashed changes
 
 DOWNLOAD_FOLDER=../data/
 PREFIX=https://rijsbergen.hum.uva.nl/david/
 DOWNLOAD_FILE=download_file_list
 
-while read file;do
-
-	wget ${PREFIX}${file} -P $DOWNLOAD_FOLDER 
-done < $DOWNLOAD_FILE
 pip3 install -r ../requirements.txt --user
 
 while read file;do
