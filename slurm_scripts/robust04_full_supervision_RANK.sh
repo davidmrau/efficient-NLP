fb7ceeb043f000dde4979c4c85a8b5ca5e3bb8c5
davidmrau <david.rau@hotmail.de>
2020-08-12 14:38:28 +0200

    add full supervision rank

---
+++ slurm_scripts/robust04_full_supervision_RANK.sh
@@ -0,0 +1,26 @@
+DATASET='robust04'
+STOPWORDS='lucene'
+MODEL='rank'
+EMBEDDING="glove"
+BATCH_SIZE=64
+
+TRAIN_SAMPLES="1000"
+VAL_SAMPLES="10000"
+NUM_EPOCHS="10000"
+
+HIDDEN="128"
+
+
+SAMPLER='uniform'
+
+SAMPLES_PER_QUERY=-1
+
+
+
+
+echo 'running'
+python3 main_full_supervision.py model=${MODEL} batch_size_test=${BATCH_SIZE} batch_size_train=${BATCH_SIZE} embedding=${EMBEDDING} \
+rank_model.hidden_sizes=${HIDDEN} dataset=${DATASET} samples_per_epoch_train=${TRAIN_SAMPLES} \
+samples_per_epoch_val=${VAL_SAMPLES} stopwords=${STOPWORDS} num_epochs=${NUM_EPOCHS} num_workers=1 samples_per_query=${SAMPLES_PER_QUERY} sampler=${SAMPLER} \
+num_workers=1 patience=20 rank_model.weights='uniform' lr=0.0001 max_samples_per_gpu=32 
+
