d7e26017741906e7397a7fc8805cd4eb962632fd
kondilidisn <kondilidisn9@gmail.com>
2020-09-16 12:51:14 +0200

    minor fix on ranks parameters

--- slurm_scripts/rank_robust04_weak_overfitting_uni_695.sh
+++ slurm_scripts/rank_robust04_weak_overfitting_uni_695.sh
@@ -25,8 +25,8 @@ TEST_TOP_2000='data/robust04/weak_overfitting/695/top_2000.csv'
 SAMPLER="uniform"
 
 
-python3 main.py model=${MODEL} batch_size_train=${BATCH_SIZE} embedding=${EMBEDDING} rank.weights="uniform" \
-rank.hidden_sizes=${HIDDEN} dataset=${DATASET} samples_per_epoch_train=${TRAIN_SAMPLES} \
+python3 main.py model=${MODEL} batch_size_train=${BATCH_SIZE} embedding=${EMBEDDING} rank_model.weights="uniform" \
+rank_model.hidden_sizes=${HIDDEN} dataset=${DATASET} samples_per_epoch_train=${TRAIN_SAMPLES} \
 samples_per_epoch_val=${VAL_SAMPLES} stopwords=${STOPWORDS} num_epochs=${NUM_EPOCHS} sampler=${SAMPLER} \
 num_workers=0 patience=1000 single_sample=True  robust_ranking_results_test=$TEST_TOP_2000 robust_query_test=$QUERIES_TEST \
 robust_ranking_results_train=$TOP_2000 robust_query_train=$QUERIES_TRAIN weak_overfitting_test=True add="695"
