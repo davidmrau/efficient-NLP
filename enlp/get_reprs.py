327f38d06c7c0a8ff48fe78b64299c2c5aab43ff
davidmrau <david.rau@hotmail.de>
2020-12-14 10:59:05 +0100

    update get_reprs

--- enlp/get_reprs.py
+++ enlp/get_reprs.py
@@ -1,7 +1,7 @@
 import numpy as np
-
+import collections
 np.random.seed(2342)
-def reprs_bert_interaction(model, dataloader, device, n_sample=5):
+def reprs_bert_interaction(model, dataloader, device, n_samples=5):
 
 	attentions = []
 	dataloader.reset()
@@ -9,19 +9,23 @@ def reprs_bert_interaction(model, dataloader, device, n_sample=5):
 	while True:
 		count = 0
 		# for q_id, data_q, length_q, batch_ids_d, batch_data_d, batch_lengths_d in dataloader.batch_generator_bert_interaction():
-		q_attentions = list()
+		q_attentions = collections.defaultdict(list)
 		for q_id, d_batch_ids, batch_input_ids, batch_attention_masks, batch_token_type_ids in dataloader.batch_generator_bert_interaction():
 			# move data to device
 			# propagate data through model
 			out, attention = model(batch_input_ids, batch_attention_masks, batch_token_type_ids, output_attentions=True)
-			q_attentions.append([l.detach().cpu().numpy() for l in attention])
-			count += 1
-		rand_ints = np.random.randint(0, high=len(q_attentions), n_sample)
-		print(rand_ints)
-		q_attentions = q_attentions[rand_ints]	
-		q_attentions = np.concatenate(q_attentions, 1)
-		print(q_attentions.shape)
-		attentions.append(q_attentions)
+			for i, l in enumerate(attention):
+				l = l.detach().cpu().numpy()
+				for e in l:
+					q_attentions[i].append(e)
+			count += 1 
 		if count == 0:
 			break
+		indices = range(len(q_attentions[0]))
+		rand_ints = np.random.choice(indices, size=n_samples, replace=False)
+		for k, v in q_attentions.items():
+			print(len(q_attentions[k]))
+			q_attentions[k] = q_attentions[k][rand_ints]	
+		attentions.append(q_attentions)
+
 	return attentions
