3e20c3148271255b8458493beea71fec2c2281f5
David Rau <david.rau@hotmail.de>
2021-01-13 23:42:51 +0100

    fix top-k ranking results

--- util_scripts/top_k_ranking_run.py
+++ util_scripts/top_k_ranking_run.py
@@ -5,18 +5,24 @@ parser.add_argument('--ranking', type=str, required=True)
 parser.add_argument('--rank', type=int, required=True)
 args = parser.parse_args()
 
-ranking_run_dict ={}
-
+prev_q_id = None
+processed = 0
 
 with open(f'{args.ranking}_{args.rank}', 'w') as out_f:
 	with open(args.ranking, 'r') as rank_run:
-		lines = rank_run.readlines()
-		for line in lines:
+		count = 0
+		for line in rank_run:
 			split = line.strip().split()
 			q_id, doc_id  = split[0], split[2]
-			if q_id not in ranking_run_dict:
-				ranking_run_dict[q_id] = [doc_id]
-				out_f.write(line)			
-			elif q_id in ranking_run_dict and len(ranking_run_dict[q_id]) < args.rank:
-				ranking_run_dict[q_id].append(doc_id)
-				out_f.write(line)			
+			if prev_q_id == None:
+				prev_q_id = q_id
+			if q_id != prev_q_id:
+				count = 0
+				processed += 1
+				if processed % 1000 == 0:
+					print(f'Processed: {processed}...')
+			if count < args.rank:
+				out_f.write(line)
+			count += 1
+			prev_q_id = q_id
+
